a2d28522dcfabfb7959aaaa0085354380e7b2282
arthmis <arthmis20@gmail.com>
2020-10-24 13:37:25 -0400

    adding better drag/drop to update recipe page

--- reshipi-frontend/js/update_recipe_components/directions.jsx
+++ reshipi-frontend/js/update_recipe_components/directions.jsx
@@ -1,74 +1,100 @@
-import {moveElementDownList, moveElementUpList} from '../utility.js';
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
 export default class Directions extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {directions: ['']};
+        this.addDirection = this.addDirection.bind(this);
+        this.updateDirections = this.updateDirections.bind(this);
+        this.removeDirection = this.removeDirection.bind(this);
+        this.dragEnd = this.dragEnd.bind(this);
+    }
+
+    addDirection(event) {
+        event.preventDefault();
+        this.setState((prevState, props) => {
+            prevState.directions.push('');
+            return (prevState);
+        });
+    }
+
+    updateDirections(index, direction) {
+        this.setState((prevState, props) => {
+            prevState.directions[index] = direction;
+            return (prevState);
+        });
+    }
+
+    removeDirection(event, index) {
+        event.preventDefault();
+        if (this.state.directions.length > 1) {
+            this.setState((prevState, props) => {
+                prevState.directions.splice(index, 1);
+                return (prevState);
+            });
+        }
+    }
+
+    dragEnd(result) {
+        if (!result.destination) {
+            return;
+        }
+        const dropIndex = result.destination.index;
+        const sourceIndex = result.source.index;
+        const directions = this.state.directions;
+
+        const [movedItem] = directions.splice(sourceIndex, 1);
+        directions.splice(dropIndex, 0, movedItem)
+        this.setState({directions});
     }
 
     render() {
         let directionList = null;
-        if (this.props.directions.length === 1) {
-            directionList = this.props.directions.map((direction, index) => {
-                return (
-                    <li className="list-item" key={index.toString()} >
-                        <div className="drag-item"
-                            draggable
-                            onDragStart={(e) => this.props.onDragStart(e, index)}
-                            onDragOver={this.props.handleDragOver}
-                            onDragOver={this.props.handleDragOver}
-                            onDrop={(e) => this.props.onDrop(e, index)}
-                        >
-                            <DirectionInput
-                                direction={direction} 
-                                updateDirections={this.props.updateDirections} 
-                                index={index} 
-                            />
-                            <span className="draggable-icon">
-                                <i className="fas fa-grip-lines"></i>
+        directionList = this.state.directions.map((direction, index) => {
+            return (
+                <Draggable key={index.toString()} draggableId={index.toString()} index={index}>
+                    {(provided) => (
+                        <li className="list-item" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <div className="drag-item">
+                                <DirectionInput
+                                    direction={direction} 
+                                    updateDirections={this.updateDirections} 
+                                    index={index} 
+                                />
+                                <span className="draggable-icon"> 
+                                    <i className="fas fa-grip-lines"></i>
+                                </span>
+                            </div>
+                            <span className="remove-input-wrapper">
+                                {this.state.directions.length === 1 ? 
+                                    <button onClick={(e) => this.removeDirection(e, index)} className="remove-input-button" disabled><i className="fas fa-times"></i></button> : 
+                                    <button onClick={(e) => this.removeDirection(e, index)} className="remove-input-button"><i className="fas fa-times"></i></button>
+                                }
                             </span>
-                        </div>
-                        <span className="remove-input-wrapper">
-                            <button onClick={(e) => this.props.removeDirection(e, index)} className="remove-input-button" disabled><i className="fas fa-times"></i></button>
-                        </span>
-                    </li>
-                );
-            });
-        } else {
-            directionList = this.props.directions.map((direction, index) => {
-                return (
-                    <li className="list-item" key={index.toString()} >
-                        <div className="drag-item"
-                            draggable
-                            onDragStart={(e) => this.props.onDragStart(e, index)}
-                            onDragOver={this.props.handleDragOver}
-                            onDragOver={this.props.handleDragOver}
-                            onDrop={(e) => this.props.onDrop(e, index)}
-                        >
-                            <DirectionInput
-                                direction={direction} 
-                                updateDirections={this.props.updateDirections} 
-                                index={index} 
-                            />
-                            <span className="draggable-icon"> 
-                                <i className="fas fa-grip-lines"></i>
-                            </span>
-                        </div>
-                        <span className="remove-input-wrapper">
-                            <button onClick={(e) => this.props.removeDirection(e, index)} className="remove-input-button"><i className="fas fa-times"></i></button>
-                        </span>
-                    </li>
-                )
-            });
-        }
+                        </li>
+                    )}
+                </Draggable>
+            )
+        });
 
         return (
             <div className="input-group">
                 <label className="label" form="new-recipe" htmlFor="directions">Directions</label><br />
-                <ol>
-                    {directionList}
-                </ol>
-                <button className="add-new-input" onClick={this.props.addDirection}>Add direction</button>
+                <DragDropContext onDragEnd={this.dragEnd}>
+                    <Droppable droppableId="directions">
+                        {(provided) => (
+                            <ul id="directions" {...provided.droppableProps} ref={provided.innerRef}>
+                                {directionList}
+                                ...
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+                <button className="add-new-input" onClick={this.addDirection}>Add direction</button>
             </div>
         );
     }
