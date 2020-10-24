e7e707e7536af306a133814d0101652d5a3487a4
arthmis <arthmis20@gmail.com>
2020-10-24 00:32:00 -0400

    adding production and dev scripts for webpack

--- reshipi-frontend/webpack.common.js
+++ reshipi-frontend/webpack.common.js
@@ -3,13 +3,23 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        new_recipe: './js/new_recipe.jsx'
+        new_recipe: './js/new_recipe.jsx',
+        edit_recipe: './js/edit_recipe.jsx'
     },
-    mode: 'development',
-    devtool: 'source-map',
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, './dist'),
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
     },
     resolve: { extensions: ['.js', '.jsx'] },
     module: {
